e3e54d5ba5c4aa714a27e18cdeda5bca242d77c9
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-17 00:18:14 +0300

    Added reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,17 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialState = {
   articles: []
 };
 
 function rootReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
